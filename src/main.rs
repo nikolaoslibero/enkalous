9d6dfc122a9660e663e997b532668230d92bf6e8
nikolaoslibero <nikolaos@violation.studio>
2024-11-13 08:41:21 -0500

    Add macroquad dependency and update main function for game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn conf() -> Conf {
+    Conf {
+        window_title: String::from("enkalous"),
+        sample_count: 2,
+        window_width: 1920,
+        window_height: 1080,
+        fullscreen: true,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(conf)]
+async fn main() {
+    #[expect(clippy::infinite_loop, reason = "this loop is the game loop")]
+    loop {
+        next_frame().await;
+    }
 }
