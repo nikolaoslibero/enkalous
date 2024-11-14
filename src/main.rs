71cc791cb88891de0420c58b54006898f9938016
nikolaoslibero <nikolaos@violation.studio>
2024-11-14 05:35:44 -0500

    Implement player and circle with command handling and rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::exit;
+
 use macroquad::prelude::*;
 
 fn conf() -> Conf {
@@ -14,8 +16,185 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
+    show_mouse(false);
+
+    let mut player = Player::new(
+        Box::new(CommandByKBM::new()),
+        Box::new(PlayerMacroquadRenderer),
+    );
+    let mut circle = Circle::new(
+        Box::new(CircleMacroquadRenderer),
+        (100.0, 100.0),
+        64.0,
+        RED,
+        1.0,
+    );
     #[expect(clippy::infinite_loop, reason = "this loop is the game loop")]
     loop {
+        clear_background(BLACK);
+
+        circle.update();
+        player.update();
+
+        circle.render();
+        player.render();
+
         next_frame().await;
+    }
+}
+
+enum PlayerCommand {
+    Aim((f32, f32)),
+    Attack,
+    Quit,
+}
+
+trait PlayerCommandInterface {
+    fn get_command(&mut self) -> Option<PlayerCommand>;
+}
+
+struct CommandByKBM;
+
+impl CommandByKBM {
+    const fn new() -> Self {
+        Self
+    }
+}
+
+impl PlayerCommandInterface for CommandByKBM {
+    fn get_command(&mut self) -> Option<PlayerCommand> {
+        if is_key_pressed(KeyCode::Escape) {
+            return Some(PlayerCommand::Quit);
+        }
+
+        if is_mouse_button_down(MouseButton::Left) {
+            return Some(PlayerCommand::Attack);
+        }
+
+        Some(PlayerCommand::Aim(mouse_position()))
+    }
+}
+
+struct CommandByAutomation {
+    command_list: Vec<PlayerCommand>,
+}
+
+impl CommandByAutomation {
+    const fn new(list: Vec<PlayerCommand>) -> Self {
+        Self { command_list: list }
+    }
+}
+
+impl PlayerCommandInterface for CommandByAutomation {
+    fn get_command(&mut self) -> Option<PlayerCommand> {
+        self.command_list.pop()
+    }
+}
+
+trait Renderable {
+    fn render(&self, position: (f32, f32));
+}
+
+struct Player {
+    command_interface: Box<dyn PlayerCommandInterface + Send>,
+    renderer: Box<dyn Renderable + Send>,
+    target: (f32, f32),
+}
+
+// SAFETY: I have no idea what I'm doing
+unsafe impl Send for Player {}
+
+struct PlayerMacroquadRenderer;
+
+impl Renderable for PlayerMacroquadRenderer {
+    fn render(&self, position: (f32, f32)) {
+        draw_circle_lines(position.0, position.1, 8.0, 2.0, BLUE);
+    }
+}
+
+struct PlayerTerminalRenderer;
+
+#[expect(clippy::print_stdout, reason = "this is a terminal renderer")]
+impl Renderable for PlayerTerminalRenderer {
+    fn render(&self, position: (f32, f32)) {
+        println!("Player is aiming at ({0}, {1})", position.0, position.1);
+    }
+}
+
+impl Player {
+    fn new(
+        command_interface: Box<dyn PlayerCommandInterface + Send>,
+        renderer: Box<dyn Renderable + Send>,
+    ) -> Self {
+        Self {
+            command_interface,
+            renderer,
+            target: (0.0, 0.0),
+        }
+    }
+    fn update(&mut self) {
+        match self.command_interface.get_command() {
+            Some(PlayerCommand::Aim(target)) => self.target = target,
+            Some(PlayerCommand::Attack) | None => {}
+            Some(PlayerCommand::Quit) => exit(0),
+        }
+    }
+    fn render(&self) {
+        self.renderer.render(self.target);
+    }
+}
+
+struct Circle {
+    renderer: Box<dyn Renderable + Send>,
+    position: (f32, f32),
+    radius: f32,
+    color: Color,
+    time_to_live: f32,
+}
+
+// SAFETY: I have no idea what I'm doing
+unsafe impl Send for Circle {}
+
+struct CircleMacroquadRenderer;
+
+impl Renderable for CircleMacroquadRenderer {
+    fn render(&self, position: (f32, f32)) {
+        draw_circle(position.0, position.1, 64.0, RED);
+    }
+}
+
+struct CircleTerminalRenderer;
+
+#[expect(clippy::print_stdout, reason = "this is a terminal renderer")]
+impl Renderable for CircleTerminalRenderer {
+    fn render(&self, position: (f32, f32)) {
+        println!(
+            "Circle is at ({0}, {1}) with radius ({2})",
+            position.0, position.1, 64.0f64
+        );
+    }
+}
+
+impl Circle {
+    fn new(
+        renderer: Box<dyn Renderable + Send>,
+        position: (f32, f32),
+        radius: f32,
+        color: Color,
+        time_to_live: f32,
+    ) -> Self {
+        Self {
+            renderer,
+            position,
+            radius,
+            color,
+            time_to_live,
+        }
+    }
+    fn render(&self) {
+        self.renderer.render(self.position);
+    }
+    fn update(&mut self) {
+        self.time_to_live -= get_frame_time();
     }
 }
